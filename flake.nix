e3c3467d6f9b8722a766657e180c69fa5904bbe6
HumXC <Hum-XC@outlook.com>
2025-03-10 13:42:06 +0800

    add nixConfig

--- flake.nix
+++ flake.nix
@@ -28,4 +28,13 @@
       };
     });
   };
+  nixConfig = {
+    # substituers will be appended to the default substituters when fetching packages
+    extra-substituters = [
+      "https://cache.garnix.io"
+    ];
+    extra-trusted-public-keys = [
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+  };
 }
